5cb668fe6d858b71eaeb6bf2b9af4c5dc6831fac
Irvin Marques de Moura <irvinmoura27@gmail.com>
2024-04-21 12:08:18 -0300

    Fix [ Ajustado os Testes de Responsividade do Header ]

--- src/__tests__/header.test.jsx
+++ src/__tests__/header.test.jsx
@@ -4,29 +4,34 @@ import '@testing-library/jest-dom';
 import { Header } from '../components/Header';
 
 describe('Header Component', () => {
-  it('renderiza corretamente', () => {
-    // Renderiza o componente Header
+  it('Renderiza o Header', () => {
+    
     render(<Header />);
 
-    // Verifica se a imagem do logotipo Hermes está presente no componente renderizado
+    const headerElement = screen.getByTestId("header");
+
+    // Verifica se a classe CSS está correta
+    expect(headerElement).toHaveClass("bg-white rounded-e-full w-fit py-[12px] pe-[5px] ps-[20px] z-10 absolute top-[20px]");
+    
     const imagemElement = screen.getByRole('img', { name: /Logotipo do Hermes/i });
     expect(imagemElement).toBeInTheDocument();
 
     // Verifica se a classe CSS está correta
     expect(imagemElement).toHaveClass('max-w-[80%]');
+    
   });
 
-  it('redimensiona a imagem corretamente em telas de médio porte', () => {
+  it('Redimensiona a imagem corretamente em telas de médio porte', () => {
     // Ajusta a largura da janela para simular uma tela de médio porte
     global.innerWidth = 768;
 
-    // Renderiza o componente Header
     render(<Header />);
 
-    // Verifica se a imagem do logotipo Hermes está presente no componente renderizado
+    const headerElement = screen.getByTestId("header");
+    expect(headerElement).toHaveClass("lg:pe-[45px] lg:ps-[120px] lg:top-[50px]")
     const imagemElement = screen.getByRole('img', { name: /Logotipo do Hermes/i });
 
-    // Verifica se a classe CSS está correta para telas de médio porte
     expect(imagemElement).toHaveClass('lg:max-w-[100%]');
+
   });
 });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import hermesLogo from "/assets/hermesLogo.svg";
 
 export function Header() {
   return (
-    <header className="bg-white rounded-e-full w-fit py-[12px] pe-[5px] lg:pe-[45px] ps-[20px] lg:ps-[120px] z-10 absolute top-[20px] lg:top-[50px]">
+    <header data-testid="header" className="bg-white rounded-e-full w-fit py-[12px] pe-[5px] lg:pe-[45px] ps-[20px] lg:ps-[120px] z-10 absolute top-[20px] lg:top-[50px]">
       <a href="./index.html"><img src={hermesLogo} alt="Logotipo do Hermes" className="max-w-[80%] lg:max-w-[100%]" /></a>
     </header>
   );
